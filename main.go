6a3cfb6168070aeabbbf2c81d8e87a16151e77ef
hikmahnurarifah <hikmahnurarifah19@gmail.com>
2024-01-08 20:19:24 +0700

    iya

--- main.go
+++ main.go
@@ -13,22 +13,27 @@ import (
 
 func main() {
 	database.Connect()
+
 	err := godotenv.Load()
 	if err != nil {
 		log.Fatal("Error loading .env files")
 	}
+
 	port := os.Getenv("PORT")
 	app := fiber.New()
 
 	app.Use(cors.New(cors.Config{
-		AllowOrigins:     string{":" + port}, // Sesuaikan dengan origin frontend Anda
+		AllowOrigins:     []string{"http://localhost:" + port}, // Sesuaikan dengan origin frontend Anda
 		AllowMethods:     "GET,POST,PUT,DELETE",
 		AllowCredentials: true,
 		AllowHeaders:     "Origin, Content-Type, Accept, Authorization",
 		ExposeHeaders:    "Authorization",
 	}))
 
-	routes.Setup(app)
-	app.Listen(":" + port)
+	go func() {
+		routes.Setup(app)
+		app.Listen(":" + port)
+	}()
 
+	select {}
 }
