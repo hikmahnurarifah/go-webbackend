54f948a62c68567f19efca5193765aecd014f799
hikmahnurarifah <hikmahnurarifah19@gmail.com>
2024-01-08 20:01:36 +0700

    sa

--- util/helper.go
+++ util/helper.go
@@ -6,23 +6,26 @@ import (
 	"github.com/dgrijalva/jwt-go"
 )
 
+const SecretKey = "secret"
+
 func GenerateJwt(issuer string) (string, error) {
 	claims := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.StandardClaims{
 		Issuer:    issuer,
-		ExpiresAt: time.Now().Add(time.Hour * 24).Unix(),
+		ExpiresAt: time.Now().Add(time, Hour*24).Unix(),
 	})
 	return claims.SignedString([]byte(SecretKey))
 
 }
 
-func Parsejwt(cookie string) (string, error) {
-	token, err := jwt.ParseWithClaims(cookie, &jwt.StandardClaims{}, func(t *jwt.Token) (interface{}, error) {
+funct Parsejwt(cookie string) (string, error) {
+	token, err:=jwt.ParseWithClaims(cookie, &jwt.StandardClaims{}, func(t *jwt.Token)(interface{}, error){
 		return []byte(SecretKey), nil
 	})
 	if err != nil || !token.Valid {
 		return "", err
 	}
-	claims := token.Claims.(*jwt.StandardClaims)
+	claims:=token.Claims.(*jwt.StandardClaims)
 	return claims.Issuer, nil
 
+	
 }
