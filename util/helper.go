436a651a9cdc649e534470aa642240eb60445461
hikmahnurarifah <hikmahnurarifah19@gmail.com>
2024-01-08 19:59:32 +0700

    hadeh

--- util/helper.go
+++ util/helper.go
@@ -6,29 +6,23 @@ import (
 	"github.com/dgrijalva/jwt-go"
 )
 
-var SecretKey = []byte("secret") // Ganti "your-secret-key" dengan kunci rahasia yang aman
-
 func GenerateJwt(issuer string) (string, error) {
 	claims := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.StandardClaims{
 		Issuer:    issuer,
 		ExpiresAt: time.Now().Add(time.Hour * 24).Unix(),
 	})
-	return claims.SignedString(SecretKey)
+	return claims.SignedString([]byte(SecretKey))
+
 }
 
-func Parsejwt(cookie string) (*jwt.StandardClaims, error) {
+func Parsejwt(cookie string) (string, error) {
 	token, err := jwt.ParseWithClaims(cookie, &jwt.StandardClaims{}, func(t *jwt.Token) (interface{}, error) {
-		return SecretKey, nil
+		return []byte(SecretKey), nil
 	})
-
 	if err != nil || !token.Valid {
-		return nil, err
+		return "", err
 	}
+	claims := token.Claims.(*jwt.StandardClaims)
+	return claims.Issuer, nil
 
-	claims, ok := token.Claims.(*jwt.StandardClaims)
-	if !ok {
-		return nil, jwt.ErrInvalidClaimsType // Sesuaikan dengan error handling yang sesuai
-	}
-
-	return claims, nil
 }
